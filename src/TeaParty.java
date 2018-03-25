54a2958644ad8ac126eac21fbe6fe2d41d68a113
agudi24 <league@WTS5.attlocal.net>
2018-03-25 16:00:52 -0700

    TeaParty

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -3,58 +3,27 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TeaParty {
-	private String name;
-	private boolean isWoman;
-	private boolean isMan;
-	private boolean isKnighted;
-	 
 	
-	public String getName() {
-		return this.name;
-	}
-	
-	public void getName(String name) {
-		this.name = name;
-	}
-	
-	public boolean getisWoman() {
-		return this.isWoman;
-	}
-	
-	public void getisWoman(boolean isWoman) {
-		this.isWoman = isWoman;
-	}
-	
-	public boolean getisMan() {
-		return this.isMan;
-	}
-	
-	public void getisMan(boolean isMan) {
-		this.isMan = isMan;
-	}
-	
-	public boolean getisKnighted() {
-		return this.isKnighted;
-	}
-	
-	public void getisKnighted(boolean isKnighted) {
-		this.isKnighted = isKnighted;
-	}
 	
 	public String welcome(String name, boolean isWoman, boolean isKnighted) {
-		if (isKnighted) {
 		if (isWoman) {
-				return "Hello Ms." + name;
-				
+			if(isKnighted) {
+				return "Hello Sir " + name;
 			}
-			else  {
-				System.out.println("Sir" + name);
-				return "Hello Sir" + name;
+			if(!isKnighted) {
+				return "Hello Ms. " + name;
+			}
+			
+		}
+		if(!isWoman) {
+			if(isKnighted) {
+				return "Hello Sir " + name;
+			}
+			if(!isKnighted) {
+				return "Hello Mr. " + name;
 			}
 		}
-		else {
-			return "Hello" + name;
-		}
+		return name;
 		
     }
 	
